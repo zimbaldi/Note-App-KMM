6279587dc6abb8ac1b60ed82fe4ec05b01f9a41c
zimbaldi <italo.zimbaldi@yahoo.com.br>
2022-10-06 21:51:31 -0300

    Creating screen and items with Jetpack Compose

--- androidApp/src/main/java/com/zimba/noteappkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/zimba/noteappkmm/android/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.zimba.noteappkmm.Greeting
+import com.zimba.noteappkmm.android.note_list.NoteListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
 @Composable
 fun MyApplicationTheme(
@@ -57,31 +55,15 @@ fun MyApplicationTheme(
     )
 }
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting(Greeting().greeting())
-                }
+                NoteListScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Hello, Android!")
-    }
-}
---
+++ androidApp/src/main/java/com/zimba/noteappkmm/android/note_list/NoteListState.kt
@@ -0,0 +1,9 @@
+package com.zimba.noteappkmm.android.note_list
+
+import com.zimba.noteappkmm.domain.note.Note
+
+data class NoteListState(
+    val notes: List<Note> = emptyList(),
+    val searchText: String = "",
+    val isSearchActive: Boolean = false
+)
