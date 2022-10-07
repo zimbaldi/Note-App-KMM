f578a6b63c732f10265747569d671c145aa3ec42
zimbaldi <italo.zimbaldi@yahoo.com.br>
2022-10-07 18:24:56 -0300

    Creating iOS app

---
+++ iosApp/iosApp/note_list/NoteListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  NoteListScreen.swift
+//  iosApp
+//
+//  Created by Italo Zimbaldi on 07/10/22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListScreen()
+    }
+}
